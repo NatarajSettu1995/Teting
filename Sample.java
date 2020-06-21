71dd1d5e1c077679317fc4b09dfeb7e6cc0da5e3
NatarajSettu1995 <natarajsettu1995@gmail.com>
2020-06-21 20:29:12 +0530

    New Add

---
+++ Sample.java
@@ -0,0 +1,35 @@
+
+
+public class Sample {
+
+	public static void main(String[] args) {
+		String Words="Wellcome";
+		String Word=Words.toLowerCase();
+		char[] array=Word.toCharArray();
+		int Wcount=0;
+		int Ccount=0;
+		String w=null;
+		String c=null;
+		for(char ret:array)
+		{
+			if(ret=='a'||ret=='e'||ret=='i'||ret=='o'||ret=='u')
+			{
+			Wcount++;
+			w=w+ret;
+			
+			}
+			else
+			{
+				Ccount++;
+				c=c+ret;
+			}
+			
+			
+		}
+		
+		System.out.println("Wovel count="+Wcount+"="+w);
+		System.out.println("Consonent count="+Ccount+"="+c);
+
+	}
+
+}
